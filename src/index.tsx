0ce96f8c998181f730c70533141c3991a55d4049
Andrey Khokhlov <progeat@mail.ru>
2025-08-09 17:01:53 +0300

    feat: add store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import {MainApp} from './apps/MainApp';
+import { MainApp } from './apps/MainApp';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <MainApp />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
