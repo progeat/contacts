cea752ef1fc7590e6ba9e090daffec65ef9a4c12
Andrey Khokhlov <progeat@mail.ru>
2025-08-31 14:40:01 +0300

    refactor: remove redux

--- src/store/groups/index.ts
+++
@@ -1,6 +0,0 @@
-import { groupContactsApi } from './slice';
-
-export const groupContactsReducer = groupContactsApi.reducer;
-export const groupContactsReducerPath = groupContactsApi.reducerPath;
-
-export const { useGetGroupContactsQuery } = groupContactsApi;
